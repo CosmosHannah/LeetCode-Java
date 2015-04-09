4623e8abc5169e3cc383f57d3da22061ba6f134a
guo7711 <mguo@esri.com>
2015-04-09 15:46:24 -0700

    #191 Number of 1 Bits
    I agree more with the commented solution. But it can not handle the test
    case 10000000000000000000000000000000

---
+++ src/leetcode/solution/Numberof1Bits191.java
@@ -0,0 +1,20 @@
+package leetcode.solution;
+
+/**
+ * Created by guo7711 on 4/9/2015.
+ */
+public class Numberof1Bits191 {
+
+    public int hammingWeight(int n) {
+
+        //  int count =0;
+        //while (n>0)
+        //{
+        //  if((n&1)==1) count++;
+        //    n >>=1;
+        //    }
+        //  return count;
+
+        return Integer.bitCount(n);
+    }
+}
