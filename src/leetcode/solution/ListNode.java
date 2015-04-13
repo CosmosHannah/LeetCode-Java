24d920644b1d9a10d26a197bed045a53cd850f2b
guo7711 <mguo@esri.com>
2015-04-13 16:43:21 -0700

    Add ListNode Class for LinkedList

---
+++ src/leetcode/solution/ListNode.java
@@ -0,0 +1,15 @@
+package leetcode.solution;
+
+/**
+ * Created by guo7711 on 4/13/2015.
+ */
+
+
+public class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) {
+                 val = x;
+                 next = null;
+             }
+     }
