b01bc551c83df076fb22c85e18da9245270a13bc
guo7711 <mguo@esri.com>
2015-04-14 10:34:21 -0700

    #100 Same Tree

---
+++ src/leetcode/solution/SameTree.java
@@ -0,0 +1,25 @@
+package leetcode.solution;
+
+/**
+ * Created by guo7711 on 4/14/2015.
+ */
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null) return true;
+        else if(p==null||q==null) return false;
+        else
+        {
+            if(p.val==q.val)
+            {
+                if(isSameTree(p.left,q.left)&&isSameTree(p.right,q.right))
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+
+    }
+}
