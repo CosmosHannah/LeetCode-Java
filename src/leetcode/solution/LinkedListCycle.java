4f4406da56bfaedf187da4322f4d5e39c9dce88a
guo7711 <mguo@esri.com>
2015-04-14 09:49:43 -0700

    #141 Linked List Cycle
    Accepted on the first try!

--- src/leetcode/solution/LinkedListCycle.java
+++ src/leetcode/solution/LinkedListCycle.java
@@ -4,4 +4,24 @@ package leetcode.solution;
  * Created by guo7711 on 4/14/2015.
  */
 public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+
+        if(head==null||head.next==null) return false;
+
+        ListNode faster = head;
+        ListNode slower = head;
+
+        while(faster!=null&&faster.next!=null)
+        {
+            faster = faster.next;
+            faster = faster.next;
+            slower = slower.next;
+            if(faster==slower) return true;
+
+        }
+
+        return false;
+
+    }
 }
