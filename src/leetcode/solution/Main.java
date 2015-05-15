878ecffacdc98d9687dc97a392e8664fcc7c7cff
mengyi <rachel.mengyi@gmail.com>
2015-05-15 15:01:58 -0700

    Main.java Test

--- src/leetcode/solution/Main.java
+++ src/leetcode/solution/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] num = new int[1];
-        num[0] = 1;
-        Permutations.permute(num);
+    //    int[] num = new int[1];
+      //  num[0] = 1;
+        //Permutations.permute(num);
 
+        int[][] a = new int[1][1];
+        a[0][0] = 0;
+        UniquePathsII.uniquePathsWithObstacles(a);
     }
 }
