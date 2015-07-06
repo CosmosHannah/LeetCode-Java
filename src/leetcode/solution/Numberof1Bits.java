818f4333ee39dc1bed750dd01dca8509bd3aac86
Yuhang Guo <hannah.yhg@gmail.com>
2015-07-05 18:02:42 -0700

    Runtime Improved #191 Number of 1 Bits
    Improved algorithm

--- src/leetcode/solution/Numberof1Bits.java
+++ src/leetcode/solution/Numberof1Bits.java
@@ -4,17 +4,15 @@ package leetcode.solution;
  * Created by guo7711 on 4/9/2015.
  */
 public class Numberof1Bits {
-
+    // public int hammingWeight(int n) {
+    //     return Integer.bitCount(n);
+    // }
     public int hammingWeight(int n) {
-
-        //  int count =0;
-        //while (n>0)
-        //{
-        //  if((n&1)==1) count++;
-        //    n >>=1;
-        //    }
-        //  return count;
-
-        return Integer.bitCount(n);
+        char [] a = Integer.toBinaryString(n).toCharArray();
+        int count = 0;
+        for(int i = 0; i< a.length; i++){
+    	    if(a[i] == '1') count++;
+        }
+        return count;
     }
 }
